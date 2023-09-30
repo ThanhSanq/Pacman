ea7cdaea0448b2de7c6b8da4fd47d7c24b054a07
ThanhSanq <thanhsang11a13@gmail.com>
2023-09-30 23:09:13 +0700

    add PacmanProject1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,18 +15,33 @@ public class GameManager : MonoBehaviour
     public int ghostMutiplier { get; private set; } = 1;
     public int score { get; private set; }
     public int lives { get; private set; }
+    public int highScore { get; private set; }
+    public int maxScore { get; private set; }
 
     public TMP_Text livesDisplay;
     public TMP_Text scoreDisplay;
+    public TMP_Text highScoreDisplay;
+
+    public AudioSource pelletMusic;
+    public AudioSource powerPelletMusic;
+    public AudioSource deathMusic;
+    public AudioSource ghostMusic;
+    public AudioSource gameOverMusic;
+    public AudioSource victoryMusic;
+
+    public GameObject pauseScreen;
+
+
+
 
     private void Awake()
     {
+        pauseScreen.SetActive(false);
         gameOver.SetActive(false);
     }
     private void Start()
     {
         NewGame();
-
 
     }
 
@@ -42,9 +57,13 @@ public class GameManager : MonoBehaviour
         livesDisplay.text = ("x " + lives.ToString());
         scoreDisplay.text = score.ToString();
         if (this.lives <= 0 && Input.anyKeyDown)
-        {
+        {            
             NewGame();
         }
+        Pausescreen();
+        CheckHighScore();
+
+
     }
 
     private void NewRound()
@@ -82,6 +101,8 @@ public class GameManager : MonoBehaviour
 
     private void GameOver()
     {
+        
+        gameOverMusic.Play();
         for (int i = 0; i < this.ghosts.Length; i++)
         {
 
@@ -95,12 +116,14 @@ public class GameManager : MonoBehaviour
 
     public void GhostEaten(Ghost ghost)
     {
+        ghostMusic.Play();
         SetScore(this.score + (ghost.points * this.ghostMutiplier));
         this.ghostMutiplier++;
     }
 
     public void PacmanEaten()
     {
+        deathMusic.Play();
         this.pacman.gameObject.SetActive(false);
 
         SetLives(this.lives - 1);
@@ -117,10 +140,12 @@ public class GameManager : MonoBehaviour
 
     public void PelletEaten(Pellet pellet)
     {
+        pelletMusic.Play();
         pellet.gameObject.SetActive(false);
         SetScore(this.score + pellet.points);
         if (!HasRemainingPellet())
         {
+            victoryMusic.Play();
             this.gameObject.SetActive(false);
             Invoke(nameof(NewRound), 3.0f);
         }
@@ -128,6 +153,7 @@ public class GameManager : MonoBehaviour
 
     public void PowerPelletEaten(PowerPellet pellet)
     {
+        powerPelletMusic.Play();
         for (int i = 0; i < ghosts.Length; i++)
         {
             this.ghosts[i].frightened.Enable(pellet.duration);
@@ -155,6 +181,30 @@ public class GameManager : MonoBehaviour
     private void ResetGhostMutiplier()
     {
         this.ghostMutiplier = 1;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseScreen.SetActive(false);
+    }
+    public void Pausescreen()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0f;
+            pauseScreen.SetActive(true);
+        }
+    }
+    private void CheckHighScore()
+    {
+        if(score > maxScore)
+        {
+            maxScore = score;
+        }
+        highScore = maxScore;
+        PlayerPrefs.SetInt("HighScore", highScore);
+        highScoreDisplay.text = ("HighScore: " + PlayerPrefs.GetInt("HighScore", highScore).ToString());
+
     }
 
 }
